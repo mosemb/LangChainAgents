1aa545146da5124b7be15829d9914b3e301964ad
mosemb <mosejava@gmail.com>
2024-01-12 17:31:57 +0100

    LangChainAgents

---
+++ langChainAgents.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 12 16:26:15 2024
+
+@author: Mose
+"""
+
+#C:\Users\Mose\Documents\Env\EnvFile.env
+# Load environment variables
+from dotenv import load_dotenv,find_dotenv
+key = load_dotenv(r'C:\Users\Mose\Documents\Env\EnvFile.env')
+print(key)
+
